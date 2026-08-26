008d5f0bcf6b4df2d65ceb077ab0c94bd5d95027
MartinMosqueira <tinchomosqueira@gmail.com>
2026-08-26 19:09:46 -0300

    💄 Interface for the default controls

--- interface.py
+++ interface.py
@@ -4,8 +4,11 @@ from PySide6.QtWidgets import (
     QWidget,
     QMainWindow,
     QVBoxLayout,
+    QHBoxLayout,
     QLabel,
     QSlider,
+    QDialogButtonBox,
+    QPushButton
 )
 
 class Windows(QMainWindow):
@@ -32,34 +35,70 @@ class Windows(QMainWindow):
         layoutControls = QVBoxLayout()
 
         # TARGET_I
-        titleLabelI = QLabel("TARGET_I")
+        titleLabelI = QLabel("Nivel Sonoro Promedio")
         titleLabelI.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layoutControls.addWidget(titleLabelI)
 
         sliderI = QSlider(Qt.Orientation.Horizontal)
-        sliderI.setRange(0, 100)
-        sliderI.setValue(0)
-        layoutControls.addWidget(sliderI)
+        sliderI.setRange(-26, -14)
+        sliderI.setValue(-16)
+
+        valueLabelI = QLabel(f"{sliderI.value()} LUFS")
+        valueLabelI.setFixedWidth(70)
+        sliderI.valueChanged.connect(lambda val: valueLabelI.setText(f"{val} LUFS"))
+
+        layoutControlsValueI = QHBoxLayout()
+
+        layoutControls.addWidget(titleLabelI)
+        layoutControlsValueI.addWidget(sliderI)
+        layoutControlsValueI.addWidget(valueLabelI)
+        layoutControls.addLayout(layoutControlsValueI)
 
         # TARGET_LRA
-        titleLabelLRA = QLabel("TARGET_LRA")
+        titleLabelLRA = QLabel("Rango Sonoro")
         titleLabelLRA.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layoutControls.addWidget(titleLabelLRA)
 
         sliderLRA = QSlider(Qt.Orientation.Horizontal)
-        sliderLRA.setRange(0, 100)
-        sliderLRA.setValue(0)
-        layoutControls.addWidget(sliderLRA)
+        sliderLRA.setRange(4, 15)
+        sliderLRA.setValue(8)
+
+        valueLabelLRA = QLabel(f"{sliderLRA.value()} LU")
+        valueLabelLRA.setFixedWidth(70)
+        sliderLRA.valueChanged.connect(lambda val: valueLabelLRA.setText(f"{val} LU"))
+
+        layoutControlsValueLRA = QHBoxLayout()
+
+        layoutControls.addWidget(titleLabelLRA)
+        layoutControlsValueLRA.addWidget(sliderLRA)
+        layoutControlsValueLRA.addWidget(valueLabelLRA)
+        layoutControls.addLayout(layoutControlsValueLRA)
 
         # TARGET_TP
-        titleLabelTP = QLabel("TARGET_TP")
+        titleLabelTP = QLabel("Picos Sonoros")
         titleLabelTP.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layoutControls.addWidget(titleLabelTP)
 
         sliderTP = QSlider(Qt.Orientation.Horizontal)
-        sliderTP.setRange(0, 100)
-        sliderTP.setValue(0)
-        layoutControls.addWidget(sliderTP)
+        sliderTP.setRange(-30, -5)
+        sliderTP.setValue(-15)
+
+        valueLabelTP = QLabel(f"{sliderTP.value()} dB")
+        valueLabelTP.setFixedWidth(70)
+        sliderTP.valueChanged.connect(lambda val: valueLabelTP.setText(f"{val} dB"))
+
+        layoutControlsValueTP = QHBoxLayout()
+
+        layoutControls.addWidget(titleLabelTP)
+        layoutControlsValueTP.addWidget(sliderTP)
+        layoutControlsValueTP.addWidget(valueLabelTP)
+        layoutControls.addLayout(layoutControlsValueTP)
+
+        layoutControlsButtons = QHBoxLayout()
+
+        cancelBtn = QPushButton("Cancelar")
+        startBtn = QPushButton("Iniciar")
+        layoutControlsButtons.addWidget(cancelBtn)
+        layoutControlsButtons.addWidget(startBtn)
+        layoutControls.addLayout(layoutControlsButtons)
+
 
         self.mainLayout.addLayout(layoutControls)
 
