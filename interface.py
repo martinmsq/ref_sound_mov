40127f22d12cf861c294c14f9ee94f6c5243110d
MartinMosqueira <tinchomosqueira@gmail.com>
2026-08-28 20:06:33 -0300

    💄 New version of controls menu

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt, QSize
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
     QWidget,
@@ -7,8 +7,8 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QLabel,
     QSlider,
-    QDialogButtonBox,
-    QPushButton
+    QPushButton,
+    QComboBox
 )
 
 class Windows(QMainWindow):
@@ -20,8 +20,12 @@ class Windows(QMainWindow):
 
         self.mainWidget = QWidget()
         self.setCentralWidget(self.mainWidget)
-
         self.mainLayout = QVBoxLayout(self.mainWidget)
+
+        # Controls
+        self.sliderI = None
+        self.sliderLRA = None
+        self.sliderTP = None
 
         # Init app
         self.get_menu()
@@ -29,27 +33,47 @@ class Windows(QMainWindow):
 
 
     def get_menu(self):
-        pass
+        options = {
+            "TV / Living": {"target_i": -16, "target_lra": 8, "target_tp": -15},
+            "Auriculares": {"target_i": -20, "target_lra": 12, "target_tp": -10},
+            "Home Cinema": {"target_i": -14, "target_lra": 9, "target_tp": -12}
+        }
+        titleMenu = QLabel("Predefinidos")
+        comboMenu = QComboBox()
+        comboMenu.addItems(list(options.keys()))
 
-    def get_controls(self):
+        comboMenu.currentTextChanged.connect(lambda option: self.apply_option(option, options))
+
+        self.mainLayout.addWidget(titleMenu)
+        self.mainLayout.addWidget(comboMenu)
+
+    def apply_option(self, option_name, options):
+        value = options.get(option_name)
+        if value is None:
+            return
+        self.sliderI.setValue(value["target_i"])
+        self.sliderLRA.setValue(value["target_lra"])
+        self.sliderTP.setValue(value["target_tp"])
+
+    def get_controls(self, target_i=-16, target_lra=8, target_tp=-15):
         layoutControls = QVBoxLayout()
 
         # TARGET_I
         titleLabelI = QLabel("Nivel Sonoro Promedio")
         titleLabelI.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        sliderI = QSlider(Qt.Orientation.Horizontal)
-        sliderI.setRange(-26, -14)
-        sliderI.setValue(-16)
+        self.sliderI = QSlider(Qt.Orientation.Horizontal)
+        self.sliderI.setRange(-26, -14)
+        self.sliderI.setValue(target_i)
 
-        valueLabelI = QLabel(f"{sliderI.value()} LUFS")
+        valueLabelI = QLabel(f"{self.sliderI.value()} LUFS")
         valueLabelI.setFixedWidth(70)
-        sliderI.valueChanged.connect(lambda val: valueLabelI.setText(f"{val} LUFS"))
+        self.sliderI.valueChanged.connect(lambda val: valueLabelI.setText(f"{val} LUFS"))
 
         layoutControlsValueI = QHBoxLayout()
 
         layoutControls.addWidget(titleLabelI)
-        layoutControlsValueI.addWidget(sliderI)
+        layoutControlsValueI.addWidget(self.sliderI)
         layoutControlsValueI.addWidget(valueLabelI)
         layoutControls.addLayout(layoutControlsValueI)
 
@@ -57,37 +81,36 @@ class Windows(QMainWindow):
         titleLabelLRA = QLabel("Rango Sonoro")
         titleLabelLRA.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        sliderLRA = QSlider(Qt.Orientation.Horizontal)
-        sliderLRA.setRange(4, 15)
-        sliderLRA.setValue(8)
+        self.sliderLRA = QSlider(Qt.Orientation.Horizontal)
+        self.sliderLRA.setRange(4, 15)
+        self.sliderLRA.setValue(target_lra)
 
-        valueLabelLRA = QLabel(f"{sliderLRA.value()} LU")
+        valueLabelLRA = QLabel(f"{self.sliderLRA.value()} LU")
         valueLabelLRA.setFixedWidth(70)
-        sliderLRA.valueChanged.connect(lambda val: valueLabelLRA.setText(f"{val} LU"))
+        self.sliderLRA.valueChanged.connect(lambda val: valueLabelLRA.setText(f"{val} LU"))
 
         layoutControlsValueLRA = QHBoxLayout()
 
         layoutControls.addWidget(titleLabelLRA)
-        layoutControlsValueLRA.addWidget(sliderLRA)
+        layoutControlsValueLRA.addWidget(self.sliderLRA)
         layoutControlsValueLRA.addWidget(valueLabelLRA)
         layoutControls.addLayout(layoutControlsValueLRA)
 
         # TARGET_TP
         titleLabelTP = QLabel("Picos Sonoros")
         titleLabelTP.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.sliderTP = QSlider(Qt.Orientation.Horizontal)
+        self.sliderTP.setRange(-30, -5)
+        self.sliderTP.setValue(target_tp)
 
-        sliderTP = QSlider(Qt.Orientation.Horizontal)
-        sliderTP.setRange(-30, -5)
-        sliderTP.setValue(-15)
-
-        valueLabelTP = QLabel(f"{sliderTP.value()} dB")
+        valueLabelTP = QLabel(f"{self.sliderTP.value()} dB")
         valueLabelTP.setFixedWidth(70)
-        sliderTP.valueChanged.connect(lambda val: valueLabelTP.setText(f"{val} dB"))
+        self.sliderTP.valueChanged.connect(lambda val: valueLabelTP.setText(f"{val} dB"))
 
         layoutControlsValueTP = QHBoxLayout()
 
         layoutControls.addWidget(titleLabelTP)
-        layoutControlsValueTP.addWidget(sliderTP)
+        layoutControlsValueTP.addWidget(self.sliderTP)
         layoutControlsValueTP.addWidget(valueLabelTP)
         layoutControls.addLayout(layoutControlsValueTP)
 
